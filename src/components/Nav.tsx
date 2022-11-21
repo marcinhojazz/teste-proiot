714a962dbb71aeff1203298dac205f88878b26dc
marcinhojazz <marcinhojazz@gmail.com>
2022-11-21 10:33:51 -0300

    update

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -22,8 +22,8 @@ const Nav = ({menuOpen}: Props) => {
       <ul className={`bg-[#005ac9] md:flex sm:items-center md:items-center pb-0 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${menuOpen}`}>
         {
           Links.map((link) => (
-            <li key={link.name} className='sm:ml-8 md:ml-8 text-xl sm:my-0 my-7 border'>
-              <a href={link.link} className='text-white hover:text-gray-400 duration-300 ease-in-out'>{link.name}</a>
+            <li key={link.name} className='sm:ml-8 md:ml-8 text-xl sm:my-0 my-7 divide-y divide-slate-100'>
+              <a href={link.link} className='text-white hover:border-b-2 hover:border-white hover:text-gray-400 duration-300 ease-in-out'>{link.name}</a>
             </li>
           ))
         }
